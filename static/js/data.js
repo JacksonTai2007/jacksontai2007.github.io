8236e328f8a1b70a2859bb875e9085fde90b8d77
初Jackson <117316518+JacksonTai2007@users.noreply.github.com>
2025-07-08 03:14:32 +0800

    refactor: simplify homepage structure

---
+++ static/js/data.js
@@ -0,0 +1,27 @@
+const tags = [
+  'IDA Pro',
+  'Python',
+  'HTML',
+  'Android',
+  'FrontEnd'
+];
+
+const timeline = [
+  { text: 'Coming Soon......', date: '2025.2' },
+  { text: '...', date: '...' },
+  { text: 'Create my first personal website', date: '2018.11' }
+];
+
+const siteProjects = [
+  { img: './static/img/i1.png', title: '', desc: '', link: '' },
+  { img: './static/img/i2.png', title: '', desc: '', link: '' },
+  { img: './static/img/i4.png', title: '', desc: '', link: '' },
+  { img: './static/img/i4.png', title: '', desc: '', link: '' }
+];
+
+const otherProjects = [
+  { img: './static/img/i1.png', title: '', desc: '', link: '' },
+  { img: './static/img/i2.png', title: '', desc: '', link: '' },
+  { img: './static/img/i4.png', title: '', desc: '', link: '' },
+  { img: './static/img/i4.png', title: '', desc: '', link: '' }
+];
